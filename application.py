8f1106613bbfd0cc852aae536461d4c47e14de1b
Guru Vignesh <guru.vignesh666@gmail.com>
2019-06-18 23:42:24 +0530

    initial query random push

--- application.py
+++ application.py
@@ -18,6 +18,25 @@ def hello_world():
     count = rows[0][0]
     return render_template('index.html', count=count)
 
+@app.route('/query_random', methods=['GET', 'POST'])
+def query_random():
+	cursor=connection.cursor()
+	query_limit = request.args['query_limit']
+	start_time = time.time()
+	list_of_times = []
+	for i in range(0, int(query_limit)):
+		start_intermediate_time = time.time()
+		select = '''select * from quakes order by rand() limit 1 '''
+		#stmt = ibm_db.prepare(db, select)
+		cursor.execute(select)
+		end_intermediate_time = time.time()
+		intermediate_time = end_intermediate_time - start_intermediate_time
+		list_of_times.append(intermediate_time)
+	end_time = time.time()
+	time_taken = (end_time - start_time) / int(query_limit)
+	return render_template('graph.html', time_taken=time_taken, list_of_times=list_of_times)
+
+
 
 if __name__ == '__main__':
     app.run()
