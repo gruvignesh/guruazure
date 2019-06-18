f07aa4692a55582e24e694ccb7b818f3c7ba4b3e
Guru Vignesh <guru.vignesh666@gmail.com>
2019-06-18 11:33:25 +0530

    db check

--- application.py
+++ application.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+from flask import Flask, render_template
+import pypyodbc
+app = Flask(__name__)
+connection = pypyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:guruearthquake.database.windows.net,1433;Database=gurudb;Uid=gurucloud;Pwd=Guruearthquake1;")
+
+@app.route('/')
+def hello_world():
+    cursor = connection.cursor()
+    cursor.execute("select count(*) from all_month")
+    rows = cursor.fetchall()
+    count = rows[0][0]
+    return render_template('index.html', count=count)
+
+
+if __name__ == '__main__':
+    app.run()
