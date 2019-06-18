a52875800de4d67f79b1dfd50fe91a71cb72c71b
Guru Vignesh <guru.vignesh666@gmail.com>
2019-06-19 00:48:48 +0530

    query rnadom8

--- application.py
+++ application.py
@@ -39,6 +39,19 @@ def query_random():
 	#list_of_times=[10,20,30]
 	return render_template('graph.html', time_taken=time_taken, list_of_times=list_of_times)
 
+@app.route('/restricted')
+def restricted():
+    query_limit = request.args['query_limit1']
+    lowmag = request.args['lowmag']
+    highermag = request.args['highermag']
+    start_time = time()
+    for i in range(0, int(query_limit)):
+        rngvalue = random.uniform(float(lowmag), float(highermag))
+        sql = 'select * from all_month where mag>=? '
+        cursor.execute(sql, (rngvalue,))
+    end_time = time()
+    time_taken = (end_time - start_time) / int(query_limit)
+    return render_template('restricted.html',time_taken=time_taken)
 
 
 if __name__ == '__main__':
