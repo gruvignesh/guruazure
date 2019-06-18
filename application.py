7b4da5ed427951b3b1afaccf8725393bf51bd691
Guru Vignesh <guru.vignesh666@gmail.com>
2019-06-18 12:35:16 +0530

    db check2

--- application.py
+++ application.py
@@ -8,7 +8,7 @@
 from flask import Flask, render_template
 import pypyodbc
 app = Flask(__name__)
-connection = pypyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:guruearthquake.database.windows.net,1433;Database=gurudb;Uid=gurucloud;Pwd=Guruearthquake1;")
+connection = pypyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:guruearthquake.database.windows.net,1433;Database=gurudb;Uid=gurucloud;Pwd=Guruearthquake1;")
 
 @app.route('/')
 def hello_world():
