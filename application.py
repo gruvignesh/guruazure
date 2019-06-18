cf8f4da10519ae991fd3a748980b0061b3da111a
Guru Vignesh <guru.vignesh666@gmail.com>
2019-06-19 00:09:24 +0530

    query rnadom5

--- application.py
+++ application.py
@@ -23,21 +23,21 @@ def hello_world():
 def query_random():
 	cursor=connection.cursor()
 	query_limit = request.args['query_limit']
-	start_time = time.time()
-	list_of_times = []
-	for i in range(0, int(query_limit)):
-		start_intermediate_time = time.time()
-		select = '''select * from quakes order by rand() limit 1 '''
-		#stmt = ibm_db.prepare(db, select)
-		cursor.execute(select)
-		end_intermediate_time = time.time()
-		intermediate_time = end_intermediate_time - start_intermediate_time
-		list_of_times.append(intermediate_time)
-	end_time = time.time()
-	time_taken = (end_time - start_time) / int(query_limit)
+	# start_time = time.time()
+	# list_of_times = []
+	# for i in range(0, int(query_limit)):
+	# 	start_intermediate_time = time.time()
+	# 	select = '''select * from quakes order by rand() limit 1 '''
+	# 	#stmt = ibm_db.prepare(db, select)
+	# 	cursor.execute(select)
+	# 	end_intermediate_time = time.time()
+	# 	intermediate_time = end_intermediate_time - start_intermediate_time
+	# 	list_of_times.append(intermediate_time)
+	# end_time = time.time()
+	# time_taken = (end_time - start_time) / int(query_limit)
 	# time_taken=89
-	# list_of_times=[10,20,30]
-	return render_template('graph.html', time_taken=time_taken, list_of_times=list_of_times)
+	list_of_times=[10,20,30]
+	return render_template('graph.html', time_taken=query_limit, list_of_times=list_of_times)
 
 
 
