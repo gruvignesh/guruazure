cd123debbb7262de5f978d637c58ce48c381414e
Guru Vignesh <guru.vignesh666@gmail.com>
2019-06-19 05:07:23 +0530

    quiz5 part

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ import random
 import pypyodbc
 #import random
 import time
-import redis
+#import redis
 # import random
 app = Flask(__name__)
 connection = pypyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:gurucloud.database.windows.net,1433;Database=gurudb;Uid=gurucloud;Pwd=Guruearthquake1;")
@@ -21,7 +21,7 @@ cache = redis.StrictRedis(host=host_name, port=6380, password=password, ssl=True
 @app.route('/')
 def hello_world():
     cursor = connection.cursor()
-    cursor.execute("select count(*) from all_month")
+    cursor.execute("select count(*) from quake6")
     rows = cursor.fetchall()
     count = rows[0][0]
     return render_template('index.html', count=count)
@@ -46,22 +46,6 @@ def query_random():
 	#list_of_times=[10,20,30]
 	return render_template('graph.html', time_taken=time_taken, list_of_times=list_of_times)
 
-# @app.route('/restricted')
-# def restricted():
-# 	cursor=connection.cursor()
-# 	query_limit = request.args['query_limit1']
-# 	lowmag = request.args['lowmag']
-# 	highermag = request.args['highermag']
-# 	start_time1 = time.time()
-# 	for i in range(0, int(query_limit)):
-# 		rngvalue = random.uniform(float(lowmag), float(highermag))
-# 		sql = 'select * from all_month where mag>=? '
-# 		cursor.execute(sql, (rngvalue,))
-# 	end_time1 = time.time()
-# 	time_taken = (end_time1 - start_time1) / int(query_limit)
-# 	#return render_template('restricted.html',time_taken=time_taken)
-# 	return render_template('restricted.html',time_taken=time_taken)
-
 @app.route('/restricted')
 def restricted():
 	cursor=connection.cursor()
@@ -71,19 +55,35 @@ def restricted():
 	start_time1 = time.time()
 	for i in range(0, int(query_limit)):
 		rngvalue = random.uniform(float(lowmag), float(highermag))
-		if not cache.get(rngvalue):
-			sql = 'select * from all_month where mag>=? '
-			cursor.execute(sql, (rngvalue,))
-			rows=cursor.fetchall()
-			cache.set(rngvalue,str(rows))
-			#flash('its in the db'+str(rngvalue))
-		else:
-			rows_string=cache.get(rngvalue)
-			#flash('its in the cache '+str(rngvalue))
+		sql = 'select * from all_month where mag>=? '
+		cursor.execute(sql, (rngvalue,))
 	end_time1 = time.time()
 	time_taken = (end_time1 - start_time1) / int(query_limit)
 	#return render_template('restricted.html',time_taken=time_taken)
 	return render_template('restricted.html',time_taken=time_taken)
+
+# @app.route('/restricted')
+# def restricted():
+# 	cursor=connection.cursor()
+# 	query_limit = request.args['query_limit1']
+# 	lowmag = request.args['lowmag']
+# 	highermag = request.args['highermag']
+# 	start_time1 = time.time()
+# 	for i in range(0, int(query_limit)):
+# 		rngvalue = random.uniform(float(lowmag), float(highermag))
+# 		if not cache.get(rngvalue):
+# 			sql = 'select * from all_month where mag>=? '
+# 			cursor.execute(sql, (rngvalue,))
+# 			rows=cursor.fetchall()
+# 			cache.set(rngvalue,str(rows))
+# 			#flash('its in the db'+str(rngvalue))
+# 		else:
+# 			rows_string=cache.get(rngvalue)
+# 			#flash('its in the cache '+str(rngvalue))
+# 	end_time1 = time.time()
+# 	time_taken = (end_time1 - start_time1) / int(query_limit)
+# 	#return render_template('restricted.html',time_taken=time_taken)
+# 	return render_template('restricted.html',time_taken=time_taken)
 
 if __name__ == '__main__':
     app.run()
